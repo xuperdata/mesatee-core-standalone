8074c8263a4d074b177701f7ed2def7e437d2e1c
duanbing <hibduan@gmail.com>
2020-06-16 16:59:33 +0800

    Update to sdk 1.1.2

--- mesatee_services/fns/sgx_trusted_lib/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let path = "/teaclave/build/ffi/golib";
-    let lib = "math";
-
-    println!("cargo:rustc-link-search=native={}", path);
-    println!("cargo:rustc-link-lib=static={}", lib);
-}
